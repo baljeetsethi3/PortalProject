72afce862fff9adc8d64b87c21dbbbc7ab791354
baljeetsethi3 <email>
2019-03-14 12:56:12 +0530

    14-3-19

---
+++ signup.js
@@ -0,0 +1,14 @@
+
+<html>
+<body>
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+Username: <input type="text" name="username"><br>
+Email: <input type="text" name="email"><br>
+Password: <input type="password" name="password"><br>
+Confirm Password : <input type="password" name="cp"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
