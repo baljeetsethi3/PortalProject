b6b4b933d1cb4d0b6800912687a309892f1d2cda
baljeetsethi3 <email>
2019-04-07 20:08:20 +0530

    7-4-19

--- first.js
+++ first.js
@@ -2,7 +2,6 @@ var http = require("http");
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-//var urlencodedParser = bodyParser.urlencoded({ extended: true });
 var mysql = require('mysql');
 var fs=require('fs');
 var enc=require('bcrypt');
@@ -12,6 +11,61 @@ var mid=require('./middlewarejwt.js');
 var uenp = bodyParser.urlencoded({ extended: true });
 var url = require('url');
 var uni="baljeet";
+var Pusher=require('pusher');
+var path = require('path');
+var flash = require('connect-flash');
+var say=require('say');
+var mailer=require('./mail.js');
+var randomstring = require("randomstring");
+var cookieparser=require('cookie-parser');
+var formidable = require('formidable');
+
+
+app.use(cookieparser());
+app.engine('html', require('ejs').renderFile);
+app.use(flash());
+
+app.use(bodyParser.urlencoded({ // Middleware
+    extended: true
+  }));
+  app.use(bodyParser.json());
+
+  
+app.post('/passwordsend.html',function(req,res){
+		var check="Select count(*) as c from register where username = "+'"'+ req.body.username+'"'+";";
+		//console.log(req.body.username);
+		con.query(check,function(err,row)
+		{
+			//console.log(req.body.username);
+			if(row[0].c==0)
+			{
+				var name="Invalid Username !";
+				res.render("C:/Users/dell/Desktop/EAD/project/forms/signin2.html", {name:name});
+			}
+			else
+			{
+				var findemail="Select * from register where username ="+'"' + req.body.username+'"' +";";
+				con.query(findemail,function(err,row,fields)
+				{
+				req.body.email=row[0].email;
+				var newpass=randomstring.generate(6);
+				req.body.password=newpass;
+				enc.hash(newpass,10,function(err,hash){
+				var newpass="update register set password="+'"'+hash+'"'+" where username="+'"'+req.body.username+'"'+";";
+				con.query(newpass,function(err){
+				if(err)
+					console.log(err);
+						});
+				mailer.sendmail(req);
+				var name="Your new password is sent to the registered email id";
+				res.render("C:/Users/dell/Desktop/EAD/project/forms/signin2.html",{name:name});
+				});
+				});
+			}
+		});
+		
+});
+
 
 //mysql connection
 var con = mysql.createConnection({
@@ -21,11 +75,38 @@ var con = mysql.createConnection({
 	database: "user"
 	});
 
+	
 	con.connect(function(err){
 		if(err) 
 			return console.error(err.message);
 		console.log("connected");
 	}); 
+
+
+	
+app.use(express.static('./forms'));
+
+
+app.get('/download', function(req, res){
+	
+  var file =  'C:/Users/dell/Desktop/EAD/project/Resource/';
+  res.write(file); // Set disposition and send it.
+});
+
+
+app.post("/fileupload",function(req,res)
+{
+	var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = 'C:/Users/dell/Desktop/EAD/project/Resource/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+});
 	
 //server connection
 var server = app.listen(8080, function () {
@@ -39,48 +120,40 @@ var server = app.listen(8080, function () {
 //start page
 app.get('/',function(req,res)
 {
-	fs.readFile("portal.html",function(err,data){
-		if(err)
-			throw err;
-		else
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write(data);
-			res.end();
-	});
+	res.redirect('/main1.html');
+	
 });
+
+
 
 app.use('/welcome',mid.checkToken,function(req,res){
-		res.write('Welcome!');
-		res.end();
-		//console.log("Welcome");
-		});
-
-app.use(bodyParser.urlencoded({ // Middleware
-    extended: true
-  }));
-  app.use(bodyParser.json());
-  
-//signup page
-app.get('/signup', function (req, res) {
-	fs.readFile("signup.html",function(err,data){
-		if(err)
-			throw err;
-		else
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write(data);
-			res.end();
-	});
+		//value="true";
+		//var q = url.parse(req.url, true);
+		//var qdata = q.query; 
+		//var usern =qdata.username; 
+		//res.cookie(usern,value,{expire:40000+Date.now()});
+		//var ssn=req.session;
+		//ssn.user="true";
+		res.render("C:/Users/dell/Desktop/EAD/project/forms/homepage.html");
+		say.speak('Welcome to ACM-CSS');
+		//res.write('Welcome!');
+		//res.end();
 });
 
+/*app.get('/logout',function(req,res){
+	res.clearCookie('user');
+	res.render("C:/Users/dell/Desktop/EAD/project/forms/main1.html");
+});*/
+
+
 //event for sign up
-app.post('/thank',uenp, function (req, res){
-	var name=req.body.name;
+app.post('/signup',uenp, function (req, res){
+	var name1=req.body.name;
 	var email=req.body.email;
 	var username=req.body.username;
 	var password=req.body.password;
 	var cpass=req.body.cpassword;
 	var phone=req.body.mobilno;
-	
 	if(password==cpass)
 	{
 	var check1="Select count(*) As c From register where email=" +'"'+ email+'"';
@@ -89,74 +162,39 @@ app.post('/thank',uenp, function (req, res){
 	con.query(check2,function(err,r,f){
 	if(rows[0].c>=1)
 	{
-		fs.readFile("signup.html",function(err,data){
-		if(err)
-			throw err;
-		else
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write("Invalid emailId, please sign up again");
-			res.write(data);
-			res.end();
-	});
+		var name="Invalid emailId, please sign up again";
+		 res.render("C:/Users/dell/Desktop/EAD/project/forms/signup2.html", {name:name});	
 	}
 	else if(r[0].u>=1)
 	{
-		fs.readFile("signup.html",function(err,data){
-		if(err)
-			throw err;
-		else
-			
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write("Username already Taken, please sign up again");
-			res.write(data);
-			res.end();
-	});
+		var name="Username already Taken, please sign up again";
+		 res.render("C:/Users/dell/Desktop/EAD/project/forms/signup2.html", {name:name});
 	}
 	else{
 		enc.hash(password,10,function(err,hash){
-	var q="INSERT INTO register(email,username,password,name,phone) VALUES (" + '"'+ email +'"'+ ',' +'"'+ username +'"'+ ',' +'"'+ hash +'"'+ ',' +'"'+ name +'"'+ ',' +'"'+ phone +'"'+ ")";
+	var q="INSERT INTO register(email,username,password,name,phone) VALUES (" + '"'+ email +'"'+ ',' +'"'+ username +'"'+ ',' +'"'+ hash +'"'+ ',' +'"'+ name1 +'"'+ ',' +'"'+ phone +'"'+ ")";
 		con.query(q,function(err,result)
 					{
 					console.log("added");
 					}
 				);
 		});
-	 var token = jwt.sign({username:uni},config.secret,{expiresIn:'10'});
-	//req.json({success:true,message:'Authentication successful',token:token});
-	//res.set('x-access-token',token);
-	res.redirect('/welcome?token='+token);
+	 var token = jwt.sign({username:uni},config.secret,{expiresIn:'100'});
+	
+	res.redirect('/welcome?token='+token+'&username='+username);
 	}
 	});
 	});
 	}
 	else
 	{
-		fs.readFile("signup.html",function(err,data){
-		if(err)
-			throw err;
-		else
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write("Password didn't match, please sign up again");
-			res.write(data);
-			res.end();
-	});
+		var name="Password did not match, please sign up again";
+		 res.render("C:/Users/dell/Desktop/EAD/project/forms/signup2.html", {name:name});
 	}
 });
 
 
-
-app.get('/signin', function (req, res) {
-	fs.readFile("signin.html",function(err,data){
-		if(err)
-			throw err;
-		else
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write(data);
-			res.end();
-	});
-});
-
-app.post('/thank2', uenp,function (req, res){
+app.post('/signin', uenp,function (req, res){
 	var username=req.body.username;
 	var password=req.body.password;
 	
@@ -164,15 +202,8 @@ app.post('/thank2', uenp,function (req, res){
 	con.query(check1,function(err,rows,fields){
 		if(rows[0].c!=1)
 	{
-		fs.readFile("signin.html",function(err,data){
-		if(err)
-			throw err;
-		else
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write("Invalid Username, please sign in again");
-			res.write(data);
-			res.end();
-		});
+		var name="Invalid Username, please sign in again";
+		 res.render("C:/Users/dell/Desktop/EAD/project/forms/signin2.html", {name:name});
 	}
 	else
 	{
@@ -181,27 +212,54 @@ app.post('/thank2', uenp,function (req, res){
 		enc.compare(password,r[0].password,function(err,res1){
 		if(!res1)
 		{
-			fs.readFile("signin.html",function(err,data){
-		if(err)
-			throw err;
-		else
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write("Incorrect Password, please sign in again");
-			res.write(data);
-			res.end();
-			});
+			var name="Incorrect Password, please sign in again";
+		 res.render("C:/Users/dell/Desktop/EAD/project/forms/signin2.html", {name:name});
 		}
 		else
 		{
 			var token = jwt.sign({username: uni},config.secret,{expiresIn:'10'});
-	//req.json({token:token});
-	//res.headers.add("x-access-token",token);
-	//console.log(token);
-	//console.log(res.headers['x-access-token']);
-res.redirect('/welcome?token='+token);
+		res.redirect('/welcome?token='+token+'&username='+username);
 		}
 		});
 	});
 	}
 });
 });
+
+var pusher = new Pusher({
+  appId: '743803',
+  key: 'ae1ad0b049515ea28202',
+  secret: '9d9e9934d6b8166754d8',
+  cluster: 'ap2',
+  encrypted: true
+});
+
+//app.post('/resource.html',function(req,res){
+	
+	
+/*
+app.get('/commentbox',function(req,res){
+	fs.readFile('commentbox.html',function(err,data){
+		if(err)
+			throw(err);
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		res.end();
+	});
+});
+	
+app.post('/welcome', function(req, res){
+	  var q = url.parse(req.url, true);
+var qdata = q.query;  
+  var username =qdata.username; 
+  console.log(req.body);
+  console.log(username);
+
+  var newComment = {
+    Query_type: req.body.type,
+    Query: req.body.comment
+  }
+  
+  pusher.trigger('comments', 'new_comment', newComment);
+  res.json({ created: true });
+});*/
